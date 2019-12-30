26f2bb59064d9106df12e30689a947e223be5d59
lassouz4 <lais.souza1962@gmail.com>
2019-12-29 21:43:09 -0300

    pages

---
+++ precache-manifest.d68301191fe5e0045e341ef4c79c2eca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669f77ef9be1d3d219dfeb96b9865817",
+    "url": "/react-todo-pages/index.html"
+  },
+  {
+    "revision": "83f6bc8592fd58ac852e",
+    "url": "/react-todo-pages/static/css/main.49fe5b25.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/react-todo-pages/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "83f6bc8592fd58ac852e",
+    "url": "/react-todo-pages/static/js/main.6da02893.chunk.js"
+  },
+  {
+    "revision": "5fd7fe22f3e7a4aea29a",
+    "url": "/react-todo-pages/static/js/runtime~main.7e9341e7.js"
+  }
+]);
